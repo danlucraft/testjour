b37aa98cab52250cb87210d85c81a09e1f0d36de
Daniel Lucraft <dan@fluentradical.com>
2009-04-09 09:44:34 +0100

    Added user to remote ssh call

--- lib/testjour/commands/run.rb
+++ lib/testjour/commands/run.rb
@@ -66,13 +66,13 @@ module Commands
     
     def start_remote_slave(remote_slave)
       uri = URI.parse(remote_slave)
-      cmd = remote_slave_run_command(uri.host, uri.path)
+      cmd = remote_slave_run_command(uri.user, uri.host, uri.path)
       Testjour.logger.info "Starting remote slave: #{cmd}"
       detached_exec(cmd)
     end
     
-    def remote_slave_run_command(host, path)
-      "ssh #{host} #{Config::CONFIG["bindir"]}/testjour run:remote --in=#{path} #{configuration.run_slave_args.join(' ')} #{testjour_uri}".squeeze(" ")
+    def remote_slave_run_command(user, host, path)
+      "ssh #{user}@#{host} /opt/songkick/bin/testjour run:remote --in=#{path} #{configuration.run_slave_args.join(' ')} #{testjour_uri}".squeeze(" ")
     end
     
     def start_slave
